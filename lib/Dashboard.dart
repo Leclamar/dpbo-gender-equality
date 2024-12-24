cb770c202f9937b71404fefac5ee715cd30e2b69
Leclamar <macellacalista@gmail.com>
2024-12-24 13:29:19 +0700

    ganti-nama

--- lib/Dashboard.dart
+++ lib/Dashboard.dart
@@ -36,7 +36,7 @@ class HomePage extends StatelessWidget {
                 ),
                 SizedBox(width: 10),
                 Text(
-                  'Full Names',
+                  'Clorinde',
                   style: TextStyle(color: Colors.white),
                 ),
               ],
